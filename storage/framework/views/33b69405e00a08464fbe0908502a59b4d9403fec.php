a1c45e418f3cea16994e35b2b8ff4a1353df8d21
Nguyễn Văn Công <congnvc@gmail.com>
2017-02-07 22:13:08 +0700

    Add files via upload

---
+++ storage/framework/views/33b69405e00a08464fbe0908502a59b4d9403fec.php
@@ -0,0 +1,78 @@
+<div class="site-header__logo-and-account is-hidden-tablet-and-below">
+   <div class="header-logo-account">
+      <div class="grid-container">
+         <div class="header-logo-account__container">
+            <a class="header-logo-account__logo" href="index.html">
+            <span>Enesy</span>
+            </a>
+            <nav class="header-logo-account__right">
+               <ul class="header-logo-account__sundry">
+                  <!-- <li class="header-logo-account__sundry-item">
+                     <a class="header-logo-account__sundry-main-link--brand-color -rounded-corners" target="_blank" href="https://elements.envato.com/?utm_campaign=elements_mkt-headernav_beta-launch_01AUG2016&amp;utm_content=tf_headernav&amp;utm_medium=promos&amp;utm_source=#">Envato Elements</a>
+                     </li>
+                     <li class="header-logo-account__sundry-item">
+                     <a class="header-logo-account__sundry-main-link -rounded-corners" target="_self" href="//#/become-an-author">Start selling</a>
+                     </li>
+                     <li class="header-logo-account__sundry-item">
+                     <a class="header-logo-account__sundry-main-link" data-view="touchOnlyDropdown" data-dropdown-target=".js-sundry-2-dropdown" href="https://forums.envato.com">Forums</a>
+                     <div class="header-logo-account__sundry-dropdown js-sundry-2-dropdown">
+                        <ul class="hub-header-dropdown">
+                           <li>
+                              <a class="header-logo-account__sundry-sub-link" href="https://forums.envato.com">All forums</a>
+                           </li>
+                           <li>
+                              <a class="header-logo-account__sundry-sub-link" href="https://forums.envato.com/c/authoring-on-envato">Authoring on Envato</a>
+                           </li>
+                           <li>
+                              <a class="header-logo-account__sundry-sub-link" href="https://forums.envato.com/c/project-making">Project making</a>
+                           </li>
+                        </ul>
+                     </div>
+                     </li>-->
+                  <li class="header-logo-account__sundry-item">
+                     <a class="header-logo-account__sundry-main-link" data-view="touchOnlyDropdown" data-dropdown-target=".js-sundry-3-dropdown" href="#">Giới thiệu</a>
+                     <div class="header-logo-account__sundry-dropdown js-sundry-3-dropdown">
+                        <ul class="hub-header-dropdown">
+                           <li>
+                              <a class="header-logo-account__sundry-sub-link" href="#">Về Enesy</a>
+                           </li>
+                        </ul>
+                     </div>
+                  </li>
+                  <li class="header-logo-account__sundry-item">
+                     <a class="header-logo-account__sundry-main-link" href="#">Trợ giúp</a>
+                     <div class="header-logo-account__sundry-dropdown js-sundry-5-dropdown">
+                        <ul class="hub-header-dropdown">
+                           <li>
+                              <a class="header-logo-account__sundry-sub-link" href="#">Mua khóa học</a>
+                           </li>
+                           <li>
+                              <a class="header-logo-account__sundry-sub-link" href="#">Trở thành giảng viên</a>
+                           </li>
+                        </ul>
+                     </div>
+                  </li>
+               </ul>
+               <div class="header-logo-account__user-nav">
+                  <div class="header-logo-account__user-nav-item">
+                     <strong>
+                     <a class="header-logo-account__user-nav-main-link--alpha" data-view="modalAjax" href="/sign_up"><span>Trở thành giảng viên</span></a>
+                     </strong>
+                  </div>
+                  <div class="header-logo-account__user-nav-item">
+                     <a class="header-logo-account__user-nav-main-link--cart" rel="nofollow" href="//#/cart">
+                        <div class="shopping-cart-summary is-empty" data-view="cartCount">
+                           <i class="e-icon -icon-cart"></i>
+                           <span class="js-cart-summary-count shopping-cart-summary__count">0</span>
+                        </div>
+                     </a>
+                  </div>
+                  <div class="header-logo-account__user-nav-item">
+                     <a rel="nofollow" class="header-logo-account__user-nav-main-link--omega" href="/sign_in">Đăng nhập</a>
+                  </div>
+               </div>
+            </nav>
+         </div>
+      </div>
+   </div>
+</div>
