325a5069fa45e3125536959afad4e67858add547
ducnvna <ducnvna@gmail.com>
2017-02-14 17:14:32 +0700

    them param

---
+++ config/params.php
@@ -0,0 +1,53 @@
+<?php
+
+    /*
+    |--------------------------------------------------------------------------
+    | Author: Ducnv
+    |--------------------------------------------------------------------------
+    |
+    | Tao file cấu hình riêng cho dự án
+    |
+    */
+
+return [
+ 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Folder Upload
+    |--------------------------------------------------------------------------
+    |
+    | Cấu hình folder chứa ảnh / video....
+    |
+    */
+
+    'folder_upload' => str_replace(DIRECTORY_SEPARATOR.'config', '', dirname(__FILE__)).DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'upload'.DIRECTORY_SEPARATOR.'image',
+
+    'folder_upload_img' => str_replace(DIRECTORY_SEPARATOR.'config', '', dirname(__FILE__)).DIRECTORY_SEPARATOR.'public',
+
+    'folder_upload_certificate' => str_replace(DIRECTORY_SEPARATOR.'config', '', dirname(__FILE__)).DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'certificate',
+
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Folder Upload
+    |--------------------------------------------------------------------------
+    |
+    | Cấu hình url chứa ảnh / video....
+    |
+    */
+
+    'url_upload' => 'http://localhost/e-learning/public/upload/image',
+
+    'url_upload_db' => 'upload/image',
+
+    'url_upload_cer_db' => 'upload/certificate',
+
+    'url_website' => 'http://localhost/e/public',
+
+
+    'status_user' => array(1 =>"Approved",2=>'Waiting Review',3=>'In Review',4=>'Rejected' ),
+    'notification_type' => ['exam' => 'Exam', 'notice' => 'Notice'],
+    'row_page' => 20,
+
+
+];
