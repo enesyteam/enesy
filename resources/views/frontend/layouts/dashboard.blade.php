7c6cdbda21c943dc78b2f8f0cecfb37a24d357e1
ducnvna <ducnvna@gmail.com>
2017-02-15 17:07:40 +0700

    11

--- resources/views/frontend/layouts/dashboard.blade.php
+++ resources/views/frontend/layouts/dashboard.blade.php
@@ -1,122 +1,72 @@
 @extends('backend.layouts.plane')
 
 @section('body')
- <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ url ('admin/') }}">Hi  {{ Auth::user()->username }}</a>
+             <div class="page">
+               <div class="page__canvas">
+                  <div class="canvas">
+                     <div class="canvas__header">
+                        <div class="site-header">
+                           <!--Main Header-->
+                           @include('frontend.partials.header')
+                           <!--#Main Header-->
+                           <!--Sub header-->
+                           @include('frontend.partials.navbar')
+                           <!--#Sub header-->
+                        </div>
+                        <!--#site_header-->
+                     </div>
+                     <!--#Canvas header-->
+                     <!--Canvas body-->
+                     <div class="js-canvas__body canvas__body">
+                        <div class="grid-container">
+                        </div>
+                        <!--#grid container-->
+                        <!--Search-->
+                        @include('frontend.partials.searchheader')
+                        <!--#search-->
+                        <!--Content-->
+                        <div class="content-container">
+                           <div class="content-main--basic">
+                              <div class="faceted-search js-faceted-search" data-view="pjaxFacetedSearch">
+                                 <div class="grid-container">
+                                    <div data-view="guestSurveyBanner">
+                                       <div class="js-system-banner-container">
+                                          <div class="js-system-banner__guest-survey">
+                                             <div class="e-alert-box -type-success">
+                                                <div class="e-alert-box__message">
+                                                   <p class="t-body -size-m h-m0">
+                                                      Thông báo thông báo thông báo Thông báo thông báo thông báo Thông báo thông báo thông báo
+                                                   </p>
+                                                </div>
+                                                <a href="#" target="_blank" class="e-btn--3d -color-primary" id="guest-survey-link">Xem ngay!</a>
+                                                <div class="e-alert-box__dismiss">
+                                                   <a href="#" class="js-e-alert-box__survey-banner-dismiss e-alert-box__dismiss-icon dismiss-banner">
+                                                   <i class="e-icon -icon-cancel"></i>
+                                                   </a>
+                                                </div>
+                                             </div>
+                                          </div>
+                                       </div>
+                                    </div>
+                                    <div class="content-l -size-scale-tablet content-right">
+                                       <!--Courses List-->
+                                       @yield('section')
+                                       <!--#Courses List-->
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <!--#Content-->
+                        <!--Footer-->
+                        @include('frontend.partials.footer')
+                        <!--#Footer-->
+                     </div>
+                     <!--#canvas body-->
+                  </div>
+                  <!--#canvas-->
+               </div>
+               <!--#page__canvas-->
             </div>
-            <!-- /.navbar-header -->
-
-            <ul class="nav navbar-top-links navbar-right">
-                <!-- /.dropdown -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-user">
-                        <li><a href="{{ URL::to('admin/userProfile?id='.Auth::user()->id) }}"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                        </li>
-                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li><a href="{{ url ('admin/logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                    <!-- /.dropdown-user -->
-                </li>
-                <!-- /.dropdown -->
-            </ul>
-            <!-- /.navbar-top-links -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
-
-
-                         
-                    
-
-                        <li >
-                            <a href="{{ url ('admin/member') }}"><i class="glyphicon glyphicon-user"></i> Members</a>
-                        </li>
-
-
-                         <li >
-                            <a href="{{ url ('admin/category/all') }}"><i class="fa fa-list-alt fa-fw"></i> Categories</a>
-                        </li>   
-
-                 
-
-                         <li>
-                            <a href="#"><i class="fa fa-graduation-cap fa-fw"></i> Course<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a  href="{{ url ('admin/course') }}"><i class="fa fa-th-list"></i> List</a>
-                                </li>
-                                <li>
-                                    <a  href="{{ url ('admin/course/add') }}"> <i class="fa fa-plus-square"></i> Add New</a>
-                                </li>
-
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>                                                                     
-
-                        
-         
-                        <li>
-                            <a href="#"><i class="fa fa-users fa-fw"></i> Admin System<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li {{ (Request::is('*panels') ? 'class="active"' : '') }}>
-                                   <a href="{{ url ('admin/adminManager?page=1') }}"><i class="fa fa-user"></i> List:</a>
-                                </li>
-                                <li {{ (Request::is('*buttons') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('admin/addUserAdmin' ) }}"><i class="fa fa-user">+</i> Add</a>
-                                </li>
-                            </ul>
-                            
-                        </li>                                             
-
-                        
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
-
-        <div id="page-wrapper">
-			 <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">@yield('page_heading')</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-           </div>
-			<div class="row">  
-				@yield('section')
-
-            </div>
-            <!-- /#page-wrapper -->
-        </div>
-    </div>
 @stop
 
--- resources/views/frontend/layouts/plane.blade.php
+++ resources/views/frontend/layouts/plane.blade.php
@@ -2,28 +2,27 @@
 <!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
 <!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
-<html lang="en" class="no-js" xmlns="http://www.w3.org/1999/html">
+<html class="js cors svg" lang="{{ config('app.locale') }}" xmlns="http://www.w3.org/1999/html">
 <!--<![endif]-->
-<head>
-	<meta charset="utf-8"/>
-	<title>{{ $meta_title or 'Quản lý hệ thống' }} </title>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta content="width=device-width, initial-scale=1" name="viewport"/>
-	<meta content="" name="description"/>
-	<meta content="" name="author"/>
-
-	<link rel="icon" type="image/png" href="{{ asset("frontend/images/logo/logo-ghi.png") }}">
-
-	<link rel="stylesheet" href="{{ asset("backend/assets/stylesheets/styles.css") }}" />
-	<link rel="stylesheet" href="{{ asset("backend/assets/scripts/bootstrap-datepicker/css/bootstrap-datepicker.css") }}" />
-
-    <script rel="text/javascript" src="{{ asset('backend/assets/scripts/jquery-2.2.3.js') }}" ></script>
-
-</head>
+     <head>
+        <meta charset="utf-8" />
+        <title>Enesy | Đào tạo Kỹ sư chuyên nghiệp</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="Đào tạo Kỹ sư chuyên nghiệp"
+           name="description" />
+        <meta content="" name="author" />
+        <link href="{{'frontend/css/normalize.css'}}" rel="stylesheet" type="text/css" />
+        <link href="{{'frontend/css/style.css'}}" rel="stylesheet" type="text/css" />
+        <link href="{{'frontend/css/custom.css'}}" rel="stylesheet" type="text/css" />
+        <link href="{{'frontend/css/layout.css'}}" rel="stylesheet" type="text/css" />
+        <!--JqueryModal-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="{{'frontend/vender/jquery-modal/jquery.modal.js'}}" type="text/javascript" charset="utf-8"></script>
+          <script src="{{'frontend/vender/jquery-modal/highlight/highlight.pack.js'}}" type="text/javascript" charset="utf-8"></script>
+           <script type="text/javascript" charset="utf-8"> hljs.initHighlightingOnLoad(); </script>
+     </head>
 <body>
 	@yield('body')
-	<script src="{{ asset("backend/assets/scripts/frontend.js") }}" type="text/javascript"></script>
-	<script src="{{ asset('backend/assets/scripts/bootstrap-datepicker/js/bootstrap-datepicker.js') }}" rel="text/javascript" ></script>
-
 </body>
 </html>
