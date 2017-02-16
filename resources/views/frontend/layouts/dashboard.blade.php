ba823871cfeef54834708b4bbc0197ee1ac437fc
ducnvna <ducnvna@gmail.com>
2017-02-16 09:10:20 +0700

    111

--- resources/views/frontend/layouts/dashboard.blade.php
+++ resources/views/frontend/layouts/dashboard.blade.php
@@ -20,44 +20,9 @@
                      <div class="js-canvas__body canvas__body">
                         <div class="grid-container">
                         </div>
+                        @yield('section')
                         <!--#grid container-->
-                        <!--Search-->
-                        @include('frontend.partials.searchheader')
-                        <!--#search-->
-                        <!--Content-->
-                        <div class="content-container">
-                           <div class="content-main--basic">
-                              <div class="faceted-search js-faceted-search" data-view="pjaxFacetedSearch">
-                                 <div class="grid-container">
-                                    <div data-view="guestSurveyBanner">
-                                       <div class="js-system-banner-container">
-                                          <div class="js-system-banner__guest-survey">
-                                             <div class="e-alert-box -type-success">
-                                                <div class="e-alert-box__message">
-                                                   <p class="t-body -size-m h-m0">
-                                                      Thông báo thông báo thông báo Thông báo thông báo thông báo Thông báo thông báo thông báo
-                                                   </p>
-                                                </div>
-                                                <a href="#" target="_blank" class="e-btn--3d -color-primary" id="guest-survey-link">Xem ngay!</a>
-                                                <div class="e-alert-box__dismiss">
-                                                   <a href="#" class="js-e-alert-box__survey-banner-dismiss e-alert-box__dismiss-icon dismiss-banner">
-                                                   <i class="e-icon -icon-cancel"></i>
-                                                   </a>
-                                                </div>
-                                             </div>
-                                          </div>
-                                       </div>
-                                    </div>
-                                    <div class="content-l -size-scale-tablet content-right">
-                                       <!--Courses List-->
-                                       @yield('section')
-                                       <!--#Courses List-->
-                                    </div>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                        <!--#Content-->
+                        
                         <!--Footer-->
                         @include('frontend.partials.footer')
                         <!--#Footer-->
