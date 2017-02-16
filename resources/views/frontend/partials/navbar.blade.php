992c4bce8232136ebec37650d98bbc613f99af07
ducnvna <ducnvna@gmail.com>
2017-02-16 15:45:26 +0700

    11

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -18,138 +18,32 @@
                   </ul>
                </div>
             </li>
+            <?php foreach($list_cat_parent as $p=>$p_value){ 
+                  $child = isset($list_cat_child[$p_value->id]) ? $list_cat_child[$p_value->id]: array();
+                  $url_pcat = '';
+
+               ?>
             <li class="header-categories__links-item">
-               <a class="header-categories__main-link" href="#">
-               Xây dựng
+               <a class="header-categories__main-link" href="<?php echo $url_pcat; ?>">
+               {{$p_value->title}}
                </a>  
+               <?php if($child){ ?>
                <div class="header-categories__links-dropdown js-categories-0-dropdown">
                   <ul class="hub-header-dropdown">
+                     <?php foreach($child as $p=>$c_value){ 
+                        $url_ccat = '';
+                        ?>
                      <li>
-                        <a class="hub-header-dropdown__top" href="#">Học nhiều nhất</a>
+                        <a class="hub-header-dropdown__top" href="<?php echo $url_ccat; ?>">{{$c_value->title}}</a>
                      </li>
-                     <li>
-                        <a href="#">Thiết kế nhà</a>
-                     </li>
-                     <li>
-                        <a href="#">Thiết kế nhà</a>
-                     </li>
-                     <li>
-                        <a class="hub-header-dropdown__bottom" href="#">Gì đây nữa?<span class="is-visually-hidden">on CodeCanyon</span></a>
-                     </li>
+                     <?php }?>
+                     
                   </ul>
                </div>
+               <?php }?>
             </li>
-            <li class="header-categories__links-item">
-               <a class="header-categories__main-link header-categories__main-link--empty header-categories__main-link--new_label" href="#">
-               Dự toán
-               &nbsp;<span class="e-text-label -size-s -color-grey-light">Mới</span>
-               </a>
-            </li>
-            <li class="header-categories__links-item">
-               <a class="header-categories__main-link" href="#">
-               Cầu đường
-               </a>  
-               <div class="header-categories__links-dropdown js-categories-2-dropdown">
-                  <ul class="hub-header-dropdown">
-                     <li>
-                        <a class="hub-header-dropdown__top" href="#">Học nhiều nhất</a>
-                     </li>
-                     <li>
-                        <a href="#">Thiết kế cầu</a>
-                     </li>
-                     <li>
-                        <a href="#">Thiết kế đường</a>
-                     </li>
-                     <li>
-                        <a href="#">Thiết kế nút giao</a>
-                     </li>
-                  </ul>
-               </div>
-            </li>
-            <li class="header-categories__links-item">
-               <a class="header-categories__main-link" href="#">
-               Quản lý dự án
-               </a>  
-               <div class="header-categories__links-dropdown js-categories-3-dropdown">
-                  <ul class="hub-header-dropdown">
-                     <li>
-                        <a class="hub-header-dropdown__top" href="#">Học nhiều nhất</a>
-                     </li>
-                     <li>
-                        <a href="#">Dự án xây dựng</a>
-                     </li>
-                     <li>
-                        <a href="#">Dự án giao thông</a>
-                     </li>
-                  </ul>
-               </div>
-            </li>
-            <li class="header-categories__links-item">
-               <a class="header-categories__main-link" href="#">
-               Tư vấn giám sát
-               </a>  
-               <div class="header-categories__links-dropdown js-categories-4-dropdown">
-                  <ul class="hub-header-dropdown">
-                     <li>
-                        <a class="hub-header-dropdown__top" href="#">Học nhiều nhất</a>
-                     </li>
-                     <li>
-                        <a href="#">Dự án xây dựng</a>
-                     </li>
-                     <li>
-                        <a href="#">Dự án giao thông</a>
-                     </li>
-                  </ul>
-               </div>
-            </li>
-            <li class="header-categories__links-item">
-               <a class="header-categories__main-link" href="#">
-               CNTT &amp; Phần mềm
-               </a>  
-               <div class="header-categories__links-dropdown js-categories-5-dropdown">
-                  <ul class="hub-header-dropdown">
-                     <li>
-                        <a class="hub-header-dropdown__top" href="#">Học nhiều nhất</a>
-                     </li>
-                     <li>
-                        <a href="#">AutoCAD</a>
-                     </li>
-                     <li>
-                        <a href="#">3Ds Max</a>
-                     </li>
-                     <li>
-                        <a href="#">AutoDesk Civil</a>
-                     </li>
-                     <li>
-                        <a href="#">BIM</a>
-                     </li>
-                  </ul>
-               </div>
-            </li>
-            <li class="header-categories__links-item">
-               <a class="header-categories__main-link" href="#">
-               Khác
-               </a>  
-               <div class="header-categories__links-dropdown js-categories-6-dropdown">
-                  <ul class="hub-header-dropdown">
-                     <li>
-                        <a class="hub-header-dropdown__top" href="#">Học nhiều nhất</a>
-                     </li>
-                     <li>
-                        <a href="#">xxxxxxx</a>
-                     </li>
-                     <li>
-                        <a href="#">yyyyyyyyy</a>
-                     </li>
-                     <li>
-                        <a href="#">xzzzzzzzzz</a>
-                     </li>
-                     <li>
-                        <a class="hub-header-dropdown__bottom" href="#">ưdfsdfsdfsdf <span class="is-visually-hidden">on ádf</span></a>
-                     </li>
-                  </ul>
-               </div>
-            </li>
+            <?php }?>
+            
             <li class="header-categories__links-item menu-demo-only">
                <a class="header-categories__main-link" href="#">
                Html pages
