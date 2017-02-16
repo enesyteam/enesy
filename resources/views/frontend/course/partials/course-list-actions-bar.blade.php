352ef06e722958b16fa1d465f73fa66696659d53
KEN-PC\KEN <bnonline1003@gmail.com>
2017-02-16 15:31:26 +0700

    update view list course

--- resources/views/frontend/course/partials/course-list-actions-bar.blade.php
+++ resources/views/frontend/course/partials/course-list-actions-bar.blade.php
@@ -6,12 +6,12 @@
                <ul class="sc-ministats-group">
                   <li title="{{$item->num_of_learn}} học viên" class="sc-ministats-item">
                      <a href="{{$link_detail}}" rel="nofollow" class="sc-ministats sc-ministats-small sc-ministats-followers">
-                     <span class="sc-visuallyhidden">{{$item->num_of_learn}} học viên</span><span aria-hidden="true">{{$item->num_of_learn}}</span>
+                     <span class="sc-visuallyhidden">{{$item->num_of_learn!=''?number_format($item->num_of_learn):0}} học viên</span><span aria-hidden="true">{{$item->num_of_learn!=''?number_format($item->num_of_learn):0}}</span>
                      </a>
                   </li>
                   <li title="7 tracks" class="sc-ministats-item">
                      <a href="{{$link_detail}}" rel="nofollow" class="sc-ministats sc-ministats-small sc-ministats-sounds">
-                     <span class="sc-visuallyhidden">{{$item->love}} quan tâm</span><span aria-hidden="true">{{$item->love}}</span>
+                     <span class="sc-visuallyhidden">{{$item->love!=''?number_format($item->love):0}} quan tâm</span><span aria-hidden="true">{{$item->love!=''?number_format($item->love):0}}</span>
                      </a>
                   </li>
                   <li title="7 tracks" class="sc-ministats-item">
