cae9d13fdcef1411f3fcf3c239d56b952717733f
Cong <congnvc@gmail.com>
2017-02-20 22:14:16 +0700

    Create Data Seed and update html

---
+++ database/seeds/CourseTableSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Models\Course;
+use App\Http\Models\Member;
+use App\Http\Models\Category;
+
+class CourseTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       	$faker = Faker\Factory::create();
+/*
+       	//create 1000 members
+		for($i=0;$i<1000;++$i){
+			Member::create([
+					'email'		=>	$faker->lexify($string = '???????????@mail.com'),
+					'username'	=>	$faker->lexify($string = '???????'),
+					'password'	=>	'123456',
+					'status'	=>	1
+				]);
+		};
+
+		//create 3 parent categories
+		for ($i=0; $i < 3 ; ++$i) {
+			Category::create([
+				'id'				=>	1000+$i,
+				'parent_id'			=>	0,
+				'title'				=>	$faker->lexify($string = 'Mục cha ???'),
+				'alias'				=>	$faker->slug(),
+				'description'		=>	implode('',$faker->sentences(4)),
+				'create_date'		=>	time(),
+				'picture'			=>	'',
+				'create_user'		=>	'ducnvna@gmail.com',
+				'status'			=>	1
+				]);
+		}
+
+		//create 15 child categories
+		for ($i=0; $i < 15 ; ++$i) {
+			Category::create([
+				'parent_id'			=>	rand(1000, 1002),
+				'title'				=>	$faker->lexify($string = 'Mục con ???'),
+				'alias'				=>	$faker->slug(),
+				'description'		=>	implode('',$faker->sentences(4)),
+				'create_date'		=>	time(),
+				'picture'			=>	'',
+				'create_user'		=>	'ducnvna@gmail.com',
+				'status'			=>	1
+				]);
+		}
+*/
+		//create 1000 courses
+		for($i=0; $i<100; ++$i){
+			Course::create([
+					'title'			=>	$faker->sentence,
+					'introtext'		=>	implode('',$faker->sentences(8)),
+					'content'		=>	implode('',$faker->sentences(30)),
+					'cat_id'		=>	rand ( 0 , 1 ),
+					'parent_cat_id'	=>	0,
+					'mentor_id'		=>	rand ( 1 , 3 ),
+					'picture'		=>	'',
+					'alias'			=>	$faker->slug(),
+					'create_date'	=>	time(),
+					'num_of_learn'	=>	0,
+					'love'			=>	rand(1, 1000),
+					'updated_at'	=>	time()
+				]);
+		}
+    }
+}
--- resources/views/frontend/author/public-profile.blade.php
+++ resources/views/frontend/author/public-profile.blade.php
@@ -8,13 +8,52 @@
           <div class="col-md-8">
             <!-- Name -->
             <div class="form-group">
-              <legend class="text-semibold">Thông tin cá nhân</legend> <label class="text-semibold">Tên bạn:</label> <input class="f-input -type-string -width-full" placeholder="" type="text" value="Nguyễn Văn Công">
+              <legend class="text-semibold">Thông tin cá nhân</legend> 
+              <label class="text-semibold">Họ:</label> <input class="f-input -type-string -width-full" placeholder="" type="text" value="Nguyễn" required="true">
+              <label class="text-semibold">Tên đệm:</label> <input class="f-input -type-string -width-full" placeholder="" type="text" value="Văn" required="true">
+              <label class="text-semibold">Tên:</label> <input class="f-input -type-string -width-full" placeholder="" type="text" value="Công" required="true">
             </div><!-- /Name -->
+            <!-- Gender -->
+            <label class="text-semibold">Giới tính:</label>
+            <label class="radio-inline ml-10">
+              <input type="radio" name="radio-unstyled-inline-left" checked="checked">Nam
+            </label>
+            <label class="radio-inline">
+              <input type="radio" name="radio-unstyled-inline-left" checked="checked">Nữ
+            </label>
+            <label class="radio-inline">
+              <input type="radio" name="radio-unstyled-inline-left" checked="checked">Khác
+            </label><br />
+            <!-- /Gender -->
+            <!-- Date of birth -->
+            <label class="text-semibold">Ngày sinh:</label> <input class="f-input -type-string -width-full" placeholder="" type="text" value="16/07/1986" required="true">
+            <!-- /Date of birth -->
             <!-- Biography -->
-            <div class="form-group">
+            <div class="form-group mt-10">
               <label class="text-semibold">Giới thiệu:</label> 
-              <textarea class="f-textarea" cols="5" rows="5">Kỹ sư cầu đường với 10 năm kinh nghiệm thiết kế, giám sát các dự án giao thông lớn ở Việt Nam như: Dự án cầu Nhật Tân, dự án hầm đường bộ qua Đèo Cả, dự án hầm đường bộ qua Đèo Cù Mông, dự án mở rộng hầm Hải Vân, ...</textarea>
+              <textarea class="f-textarea font-size-13" cols="5" rows="10">Kỹ sư cầu đường với 10 năm kinh nghiệm thiết kế, giám sát các dự án giao thông lớn ở Việt Nam như: Dự án cầu Nhật Tân, dự án hầm đường bộ qua Đèo Cả, dự án hầm đường bộ qua Đèo Cù Mông, dự án mở rộng hầm Hải Vân, ...</textarea>
             </div><!-- /Biography -->
+            <!-- Biography help -->
+            <div class="box h-clickable biography-help mt--10 is-open" data-view="dropdown" data-dropdown-target="#us-person-definition">
+            <h3 class="t-body -size-s h-m0">
+              <i class="e-icon -icon-info -margin-right"></i>
+              <strong>Các thẻ <code>&lt;html&gt;</code> được chấp nhận</strong>
+            </h3>
+
+            <ul class="t-list mt-10 is-hidden" id="us-person-definition">
+              <li class="t-body -size-s h-m0">&lt;strong&gt;&lt;/strong&gt; để in đậm text.
+              </li>
+              <li class="t-body -size-s h-m0">&lt;ul&gt;&lt;li&gt; hoặc &lt;ol&gt;&lt;li&gt; để tạo danh sách.
+              </li>
+              <li class="t-body -size-s h-m0">&lt;a&gt;&lt;/a&gt; để chèn một liên kết.
+              </li>
+              <li class="t-body -size-s h-m0">&lt;img&gt; để chèn một hình ảnh.
+              </li>
+              <li class="t-body -size-s h-m0">&lt;blockquote&gt;&lt;/blockquote&gt; để chèn một trích dẫn.
+              </li>
+            </ul>
+            </div>
+            <!-- /Biography help -->
             <button class="btn btn-primary" type="button">Cập nhật</button>
           </div>
           <div class="col-md-1"></div>
@@ -32,4 +71,17 @@
     </form>
   </div>
 </div><!-- /content area -->
+@stop
+@section('head-scripts')
+<script type="text/javascript">
+  //this is demo by cong, need to rewrite by code team
+  $(document).ready(function() {
+  var clickArea         = $(".biography-help");
+
+  $(clickArea).click(function(e){
+      e.preventDefault();
+          $(clickArea).children('ul#us-person-definition').toggleClass("is-hidden");
+  });
+});
+</script>
 @stop
