91906771acf8d9f0b185db7f97099d65b39901f6
ducnvna <ducnvna@gmail.com>
2017-02-21 15:13:09 +0700

    1

--- database/migrations/2017_02_21_054809_create_tbl_categories.php
+++ database/migrations/2017_02_21_054809_create_tbl_categories.php
@@ -17,24 +17,18 @@ class CreateTblCategories extends Migration
                     $table->engine = 'InnoDB';
                     $table->charset = 'utf8';
                     $table->collation = 'utf8_unicode_ci';
-
                     $table->increments('id')->unsigned();
                     $table->integer('parent_id')->nullable();
-                    $table->string('mobile', 255)->nullable();
-                    $table->string('username', 255)->nullable();
-                    $table->string('password', 255)->nullable();
-                    $table->string('full_name', 255)->nullable();
-                    $table->date('updated_at', 255)->nullable();                    
-                    $table->tinyInteger('active')->unsigned()->nullable()->default(1)->index()->comment = '1:active 2:not active';
-                    $table->string('info', 4000)->nullable();
+                    $table->string('title', 255)->nullable();
+                    $table->string('alias', 255)->nullable();
+                    $table->string('description',500)->nullable();
+                    $table->string('picture', 255)->nullable();
+                    $table->string('create_user', 255)->nullable();  
                     $table->integer('create_date')->nullable();
-                    $table->string('create_user', 255)->nullable();
-                    $table->integer('modify_date')->nullable();
-                    $table->string('modify_user', 255)->nullable();
-                    $table->string('remember_token', 255)->nullable();
-                    $table->tinyInteger('type_admin')->unsigned()->nullable()->default(1)->index()->comment = '1:Supper admin:Admin';
-                    $table->integer('permission')->nullable();
-                    $table->string('avatar', 255)->nullable();
+                    $table->tinyInteger('level')->unsigned()->nullable()->default(1)->index();
+                    $table->string('ordering')->nullable()->index();
+                    $table->tinyInteger('status')->unsigned()->nullable()->default(0)->index()->comment = '1:active, 0:not active';
+    
                     
                 });
     }
