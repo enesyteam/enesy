699e6e1067d097791b4312e786f7aee906a7453f
ducnvna <ducnvna@gmail.com>
2017-02-21 16:01:24 +0700

    11

--- database/migrations/2017_02_21_054912_create_tbl_member.php
+++ database/migrations/2017_02_21_054912_create_tbl_member.php
@@ -19,11 +19,28 @@ class CreateTblMember extends Migration
                     $table->collation = 'utf8_unicode_ci';
                     
                     $table->increments('id')->unsigned();
-                    $table->integer('cat_id')->nullable();
-                    $table->integer('course_id')->nullable();
-                    $table->string('title', 255)->nullable();
+                    $table->string('email', 255)->nullable();
+                    $table->string('phone', 100)->nullable();
+                    $table->string('username', 255)->nullable();
+                    $table->string('password', 255)->nullable();
+                    $table->string('first_name', 255)->nullable();
+                    $table->string('middle_name', 255)->nullable();
+                    $table->string('last_name', 255)->nullable();
+                    $table->date('updated_at', 255)->nullable();                    
+                    $table->tinyInteger('status')->unsigned()->nullable()->default(2)->index()->comment = '1.approved 2.waiting review 3.in review 4. rejected';
+                    $table->string('info', 4000)->nullable();
                     $table->integer('create_date')->nullable();
-                    $table->integer('updated_at')->nullable();
+                    $table->string('create_user', 255)->nullable();
+                    $table->integer('modify_date')->nullable();
+                    $table->string('modify_user', 255)->nullable();
+                    $table->string('remember_token', 255)->nullable();
+                    $table->string('avatar', 255)->nullable();
+                    $table->tinyInteger('is_teaching')->unsigned()->nullable()->default(0)->index()->comment = '1. là giáo viên 0. ko phải giáo viên';
+                    $table->string('sex', 45)->nullable();
+                    $table->date(format)('dob')->nullable();
+                    $table->string('social', 255)->nullable();
+                    $table->string('address', 255)->nullable();
+                    
                     
                 });
     }
